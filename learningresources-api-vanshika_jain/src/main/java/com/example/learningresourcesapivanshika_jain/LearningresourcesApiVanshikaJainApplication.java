3b27260e9952994e430285b74a88fb123b926a26
Vanshika <vanshikajain2411@gmail.com>
2022-07-12 18:18:28 +0530

    first commit

---
+++ learningresources-api-vanshika_jain/src/main/java/com/example/learningresourcesapivanshika_jain/LearningresourcesApiVanshikaJainApplication.java
@@ -0,0 +1,13 @@
+package com.example.learningresourcesapivanshika_jain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningresourcesApiVanshikaJainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningresourcesApiVanshikaJainApplication.class, args);
+	}
+
+}
---
+++ learningresources-api-vanshika_jain/src/test/java/com/example/learningresourcesapivanshika_jain/LearningresourcesApiVanshikaJainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learningresourcesapivanshika_jain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningresourcesApiVanshikaJainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
